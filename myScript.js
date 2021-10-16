c666bd25bd17ac6d077f5460fe2a394dd2364e48
Lncn <91021063+yeahant@users.noreply.github.com>
2021-10-16 11:37:08 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -151,7 +151,6 @@ function snake() {
              * It took me a long time to understand this array.
              * console.log(this.body) => Key to problem solving
              */
-            console.log(this.body);
             this.body.push([x,y,'#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).substr(-6)]);
             // Food disappear
             this.body[this.body.length - 1][2] = "#000000";
